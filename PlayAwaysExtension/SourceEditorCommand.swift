24dac9a6e927b67d590b6fded6df866d897894fa
Guilherme Rambo <insidegui@gmail.com>
2016-12-10 13:56:32 -0200

    Set extension info

--- PlayAwaysExtension/SourceEditorCommand.swift
+++ PlayAwaysExtension/SourceEditorCommand.swift
@@ -12,7 +12,7 @@ import XcodeKit
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
-        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
         
         completionHandler(nil)
     }
